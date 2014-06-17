6341f30986ee71d95474003a1174faafdb837107
Collin Glass <collinglass@gmx.com>
2014-06-17 10:03:33 -0400

    Create, Get, Update, Delete, List DONE

--- main.go
+++ main.go
@@ -5,7 +5,7 @@
  *
  * @author Collin Glass <collin@fluidware.com>
  * @copyright 2014 Fluidware
- * @license MIT <https://raw.github.com/fluidware/fluidsurveys-node/master/LICENSE>
+ * @license MIT <https://raw.github.com/collinglass/fluidsurveys-go/master/LICENSE>
  * @link http://fluidsurveys.com
  * @version 0.1.3
  */
@@ -13,11 +13,12 @@
 package main
 
 import (
+	"bytes"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
-	"strings"
+	// "net/url"
+	"encoding/json"
 )
 
 const (
@@ -32,34 +33,49 @@ var (
 			"root":    "/{type}/",
 			"details": "/{type}/{id:[0-9]+}/",
 		},
-		"collections": map[string][]string{
-			"templates":     []string{},
-			"surveys":       []string{"collectors", "invites", "responses", "structure", "invite_codes", "groups", "reports", "csv"},
-			"collectors":    []string{},
-			"contacts":      []string{},
-			"embed":         []string{},
-			"contact-lists": []string{"contacts"},
-			"webhooks":      []string{},
+		"collections": map[string]map[string]int{
+			"templates":     map[string]int{},
+			"surveys":       map[string]int{"collectors": 1, "invites": 1, "responses": 1, "structure": 1, "invite_codes": 1, "groups": 1, "reports": 1, "csv": 1},
+			"collectors":    map[string]int{},
+			"contacts":      map[string]int{},
+			"embed":         map[string]int{},
+			"contact-lists": map[string]int{"contacts": 1},
+			"webhooks":      map[string]int{},
 		},
 	}
 	FORMAT = "" // default is json
 )
 
-func NewSurvey(name string) string {
+func Create(entityType string, data map[string]string) string {
 	client := &http.Client{}
-	URL := HOST + "surveys/"
-	if FORMAT != "" {
-		URL = URL + "&format=" + FORMAT
-	}
+	URL := HOST + entityType + "/"
 
-	v := url.Values{}
-	v.Set("name", name)
+	b, err := json.Marshal(data)
+
 	//pass the values to the request's body
-	req, err := http.NewRequest("POST", URL, strings.NewReader(v.Encode()))
+	req, err := http.NewRequest("POST", URL, bytes.NewReader(b))
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	req.Header.Set("Content-Type", "application/json")
+
+	req.SetBasicAuth(EMAIL, PASSWORD)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	s := string(bodyText)
+	return s
+}
+
+func Get(entityType, id string) string {
+	client := &http.Client{}
+	URL := HOST + entityType + "/" + id + "/"
+
+	req, err := http.NewRequest("GET", URL, nil)
 	req.SetBasicAuth(EMAIL, PASSWORD)
 	resp, err := client.Do(req)
 	if err != nil {
@@ -70,13 +86,36 @@ func NewSurvey(name string) string {
 	return s
 }
 
-func List() string {
+func Update(entityType, id string, data map[string]string) string {
 	client := &http.Client{}
-	url := HOST + "surveys/"
-	if FORMAT != "" {
-		url = url + "?format=" + FORMAT
+	URL := HOST + entityType + "/" + id + "/"
+
+	b, err := json.Marshal(data)
+
+	//pass the values to the request's body
+	req, err := http.NewRequest("PUT", URL, bytes.NewReader(b))
+	if err != nil {
+		log.Fatal(err)
 	}
-	req, err := http.NewRequest("GET", url, nil)
+
+	req.Header.Set("Content-Type", "application/json")
+
+	req.SetBasicAuth(EMAIL, PASSWORD)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	s := string(bodyText)
+	return s
+}
+
+func Delete(entityType, id string) string {
+	client := &http.Client{}
+	URL := HOST + entityType + "/" + id + "/"
+
+	req, err := http.NewRequest("DELETE", URL, nil)
 	req.SetBasicAuth(EMAIL, PASSWORD)
 	resp, err := client.Do(req)
 	if err != nil {
@@ -87,13 +126,19 @@ func List() string {
 	return s
 }
 
-func ListResponses(id string) string {
+func List(entityType string, args map[string]string) string {
 	client := &http.Client{}
-	url := HOST + "surveys/" + id + "/repsonses/"
-	if FORMAT != "" {
-		url = url + "?format=" + FORMAT
+	URL := HOST + entityType + "/"
+
+	if args != nil {
+		URL = URL + "?"
 	}
-	req, err := http.NewRequest("GET", url, nil)
+	for key, value := range args {
+		URL = URL + key + "=" + value
+	}
+	// IF args are not nil then add to query string plus add predicate, etc.
+
+	req, err := http.NewRequest("GET", URL, nil)
 	req.SetBasicAuth(EMAIL, PASSWORD)
 	resp, err := client.Do(req)
 	if err != nil {
@@ -106,6 +151,8 @@ func ListResponses(id string) string {
 
 func main() {
 
-	log.Println("Initial Commit.")
-	log.Println(NewSurvey("TESTING"))
+	log.Println("Running..")
+	// log.Println(ListResponses("550996"))
+
+	// log.Println(FilterCSVByDateUpdated("550996", "2013-11-04", ">"))
 }
